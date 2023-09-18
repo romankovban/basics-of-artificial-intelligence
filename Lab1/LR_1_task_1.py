085c3f0abafb5139c25b3745e3e78a62cd77dc5d
Roman Kovban <roman.kovban@gmail.com>
2023-09-18 23:48:54 +0300

    completed task 2 and task 3

--- Lab1/LR_1_task_1.py
+++ Lab1/LR_1_task_1.py
@@ -33,3 +33,29 @@ data_normalized_l2 = preprocessing.normalize(input_data,
                                              norm='l2')
 print("\nl1 normalized data:\n", data_normalized_l1)
 print("\nl2 normalized data:\n", data_normalized_l2)
+
+# Task 2.1.5
+# Надання позначок вхідних даних
+input_labels = ['red', 'black', 'red', 'green', 'black',
+                'yellow', 'white']
+# Створення кодувальника та встановлення відповідності
+# між мітками та числами
+encoder = preprocessing.LabelEncoder()
+encoder.fit(input_labels)
+
+# Виведення відображення
+print("Label mapping:")
+for i, item in enumerate(encoder.classes_):
+    print(item, '-->', i)
+
+# перетворення міток за допомогою кодувальника
+test_labels = ['green', 'red', 'black']
+encoded_values = encoder.transform(test_labels)
+print("\nLabels =", test_labels)
+print("Encoded values =", list(encoded_values))
+
+# Декодування набору чисел за допомогою декодера
+encoded_values = [3, 0, 4, 1]
+decoded_list = encoder.inverse_transform(encoded_values)
+print("\nEncoded values =", encoded_values)
+print("Decoded labels =", list(decoded_list))
--- Lab1/LR_2_task_2.py
+++ Lab1/LR_2_task_2.py
@@ -1,6 +1,24 @@
 import numpy as np
 from sklearn import preprocessing
 
-# Надання позначок вхідних даних
-Input_labels = ['red', 'Ыасk', 'red', 'green', 'Ьlack',
-                'yellow', 'white']
+input_data = np.array([[4.6, 3.9, -3.5],
+                       [-2.9, 4.1, 3.3],
+                       [2.2, 8.8, -4.1],
+                       [3.9, 2.4, 4.2]])
+
+data_binarized = preprocessing.Binarizer(threshold=2.2).transform(input_data)
+print("\n Binarized data:\n", data_binarized)
+
+print("\nBEFORE: ")
+print("Mean =", input_data.mean(axis=0))
+print("Std deviation =", input_data.std(axis=0))
+
+data_scaled = preprocessing.scale(input_data)
+print("\nAFTER: ")
+print("Mean =", data_scaled.mean(axis=0))
+print("Std deviation =", data_scaled.std(axis=0))
+
+data_normalized_l1 = preprocessing.normalize(input_data, norm='l1')
+data_normalized_l2 = preprocessing.normalize(input_data, norm='l2')
+print("\nl1 normalized data:\n", data_normalized_l1)
+print("\nl2 normalized data:\n", data_normalized_l2)
