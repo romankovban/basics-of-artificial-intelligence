d88ed003c6ae5ab191ae7c1c20f15f969d9ce49f
Roman Kovban <roman.kovban@gmail.com>
2023-09-18 22:02:08 +0300

    task 1

---
+++ Lab1/LR_2_task_2.py
@@ -0,0 +1,6 @@
+import numpy as np
+from sklearn import preprocessing
+
+# Надання позначок вхідних даних
+Input_labels = ['red', 'Ыасk', 'red', 'green', 'Ьlack',
+                'yellow', 'white']
