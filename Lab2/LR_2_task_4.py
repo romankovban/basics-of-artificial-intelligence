73e896a5dd627ab6e1be64f6ca0f1ef91836ba06
Roman Kovban <roman.kovban@gmail.com>
2023-10-06 21:14:52 +0300

    Lab 2: completed task 4

---
+++ Lab2/LR_2_task_4.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.svm import LinearSVC
+from sklearn.multiclass import OneVsOneClassifier
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+input_file = 'income_data.txt'
+X = []
+y = []
+count_class1 = 0
+count_class2 = 0
+max_datapoints = 25000
+
+with open(input_file, 'r') as f:
+    for line in f.readlines():
+        if count_class1 >= max_datapoints and count_class2 >= max_datapoints:
+            break
+
+        if '?' in line:
+            continue
+
+        data = line[:-1].split(', ')
+        income_class = data[-1]
+        if income_class == '<=50K' and count_class1 < max_datapoints:
+            X.append(data)
+            count_class1 += 1
+
+        if income_class == '>50K' and count_class2 < max_datapoints:
+            X.append(data)
+            count_class2 += 1
+
+X = np.array(X)
+
+label_encoder = []
+X_encoded = np.empty(X.shape)
+for i, item in enumerate(X[0]):
+    if item.isdigit():
+        X_encoded[:, i] = X[:, i]
+    else:
+        current_label_encoder = preprocessing.LabelEncoder()
+        label_encoder.append(current_label_encoder)
+        X_encoded[:, i] = current_label_encoder.fit_transform(X[:, i])
+
+X = X_encoded[:, :-1].astype(int)
+y = X_encoded[:, -1].astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto', max_iter=10000)))
+
+results = []
+names = []
+
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
