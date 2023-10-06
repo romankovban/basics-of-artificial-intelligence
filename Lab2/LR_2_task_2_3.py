cdcd78382276d502ea98c54458e493e41622f18a
Roman Kovban <roman.kovban@gmail.com>
2023-10-06 20:45:40 +0300

    Lab 2: completed task 2

---
+++ Lab2/LR_2_task_2_3.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+
+input_file = 'income_data.txt'
+X = []
+y = []
+count_class1 = 0
+count_class2 = 0
+max_datapoints = 25000
+
+with open(input_file, 'r') as f:
+    for line in f.readlines():
+        if count_class1 >= max_datapoints and count_class2 >= max_datapoints:
+            break
+
+        if '?' in line:
+            continue
+
+        data = line[:-1].split(', ')
+        income_class = data[-1]
+        if income_class == '<=50K' and count_class1 < max_datapoints:
+            X.append(data)
+            count_class1 += 1
+
+        if income_class == '>50K' and count_class2 < max_datapoints:
+            X.append(data)
+            count_class2 += 1
+
+X = np.array(X)
+
+label_encoder = []
+X_encoded = np.empty(X.shape)
+for i, item in enumerate(X[0]):
+    if item.isdigit():
+        X_encoded[:, i] = X[:, i]
+    else:
+        current_label_encoder = preprocessing.LabelEncoder()
+        label_encoder.append(current_label_encoder)
+        X_encoded[:, i] = current_label_encoder.fit_transform(X[:, i])
+
+X = X_encoded[:, :-1].astype(int)
+y = X_encoded[:, -1].astype(int)
+
+classifier = OneVsOneClassifier(SVC(kernel='sigmoid', max_iter=10000))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+classifier.fit(X_train, y_train)
+y_test_pred = classifier.predict(X_test)
+
+accuracy = cross_val_score(classifier, X, y, scoring='accuracy', cv=3)
+print("Accuracy score: " + str(round(100 * accuracy.mean(), 2)) + "%")
+
+precision = cross_val_score(classifier, X, y, scoring='precision', cv=3)
+print("Precision score: " + str(round(100 * precision.mean(), 2)) + "%")
+
+recall = cross_val_score(classifier, X, y, scoring='recall', cv=3)
+print("Recall score: " + str(round(100 * recall.mean(), 2)) + "%")
+
+f1 = cross_val_score(classifier, X, y, scoring='f1_weighted', cv=3)
+print("F1 score: " + str(round(100 * f1.mean(), 2)) + "%")
+
+input_data = ['37', 'Private', '215646', 'HS-grad', '9', 'Never-married', 'Handlers-cleaners', 'Not-in-family', 'White',
+              'Male',
+              '0', '0', '40', 'United-States']
+
+input_data_encoded = [-1] * len(input_data)
+count = 0
+for i, item in enumerate(input_data):
+    if item.isdigit():
+        input_data_encoded[i] = int(input_data[i])
+    else:
+        encoder = label_encoder[count]
+        input_data_encoded[i] = int(encoder.transform([(input_data[i])])[-1])
+        count += 1
+
+input_data_encoded = np.array(input_data_encoded)
+predicted_class = classifier.predict([input_data_encoded])
+print(label_encoder[-1].inverse_transform(predicted_class)[0])
